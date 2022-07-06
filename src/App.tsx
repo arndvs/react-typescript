a22fc8e2e34ddbddf7b5718ee51767fbef17f189
Aaron Davis <108478438+arndvs@users.noreply.github.com>
2022-07-06 14:35:06 -0700

    Fundamentals - Simple prop types

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NameTag from "./components/NameTag"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <div>
+            <h1>1. Name Tag</h1>
+            <NameTag />
+        </div>
+        <hr></hr>
+        <div>
+            <h1>2. </h1>
+
+        </div>
+        <hr></hr>
+        <div>
+            <h1>3. </h1>
+
+        </div>
+        <hr></hr>
+        <div>
+            <h1>4. </h1>
+
+        </div>
+        <hr></hr>
+        <div>
+            <h1>5. </h1>
+
+        </div>
+        <hr></hr>
+        <div>
+            <h1>6. </h1>
+
+        </div>
+        <hr></hr>
+        <div>
+            <h1>7. </h1>
+
+        </div>
+        <hr></hr>
+        <div>
+            <h1>8. </h1>
+
+        </div>
+      </div>
     </div>
   );
 }
